804cc0c011558d91d19e99c8b7c10b2a8c68681c
Jesus Rodriguez <rodriguezjesus609@gmail.com>
2020-05-24 23:55:15 +0200

    Create base project and start jwt token

---
+++ index.js
@@ -0,0 +1,42 @@
+'use strict'
+
+require('dotenv').config()
+const { makeExecutableSchema } = require('graphql-tools')
+const express = require('express')
+const cors = require('cors')
+const gqlMiddleware = require('express-graphql')
+const { readFileSync } = require('fs')
+const { join } = require('path')
+const resolvers = require('./lib/resolvers')
+const passport = require('passport')
+const jwt = require('jsonwebtoken')
+
+const app = express()
+const port = process.env.port || 3000
+const isDev = process.env.NODE_ENV.trimRight() !== 'production'
+
+
+// definir el schema
+const typeDefs = readFileSync(
+  join(__dirname, 'lib', 'schema.graphql'),
+  'utf-8'
+)
+
+const schema = makeExecutableSchema({ typeDefs , resolvers })
+
+
+app.use(cors())
+
+// Middleware defs
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use('/api', gqlMiddleware({
+  schema: schema,
+  rootValue: resolvers,
+  graphiql: isDev
+}))
+
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}/api`)
+})
