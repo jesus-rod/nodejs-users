2bba3ade680ebe5b06a7ee3357603df7c710e1b0
Jesus Rodriguez <rodriguezjesus609@gmail.com>
2020-05-25 08:18:50 +0200

    Add eslint and prettier
    npm i -D eslint eslint-config-prettier eslint-plugin-prettier prettier

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ const gqlMiddleware = require('express-graphql')
 const { readFileSync } = require('fs')
 const { join } = require('path')
 const resolvers = require('./lib/resolvers')
-const passport = require('passport')
-const jwt = require('jsonwebtoken')
+//const passport = require('passport')
 
 const app = express()
 const port = process.env.port || 3000
@@ -28,8 +27,8 @@ const schema = makeExecutableSchema({ typeDefs , resolvers })
 app.use(cors())
 
 // Middleware defs
-app.use(passport.initialize())
-app.use(passport.session())
+////app.use(passport.initialize())
+////app.use(passport.session())
 
 app.use('/api', gqlMiddleware({
   schema: schema,
